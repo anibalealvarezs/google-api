d2055faeed1f48b225fb6c58349d2e0931a8c3ee
Anibal Alvarez <aalvarez@chmw.co>
2026-04-08 18:04:27 -0400

    feat: add getAllSearchQueryResultsAndProcess method for stream-like processing

--- src/Services/SearchConsole/SearchConsoleApi.php
+++ src/Services/SearchConsole/SearchConsoleApi.php
@@ -206,6 +206,58 @@ class SearchConsoleApi extends GoogleApi
     }
 
     /**
+     * Fetches all search query results from Google Search Console and processes them with a callback.
+     *
+     * @param string $siteUrl
+     * @param string $startDate
+     * @param string $endDate
+     * @param int $rowLimit
+     * @param int $startRow
+     * @param DataState $dataState
+     * @param array|null $dimensions
+     * @param string|null $type
+     * @param array<int, DimensionFilterGroup|array>|null $dimensionFilterGroups
+     * @param AggregationType $aggregationType
+     * @param callable|null $callback
+     * @return void
+     * @throws GuzzleException
+     */
+    public function getAllSearchQueryResultsAndProcess(
+        string $siteUrl,
+        string $startDate,
+        string $endDate,
+        int $rowLimit = 25000,
+        int $startRow = 0,
+        DataState $dataState = DataState::ALL,
+        ?array $dimensions = null,
+        ?string $type = null,
+        ?array $dimensionFilterGroups = null,
+        AggregationType $aggregationType = AggregationType::AUTO,
+        ?callable $callback = null
+    ): void {
+        $params = [
+            'siteUrl' => $siteUrl,
+            'startDate' => $startDate,
+            'endDate' => $endDate,
+            'rowLimit' => $rowLimit,
+            'startRow' => $startRow,
+            'dataState' => $dataState,
+            'dimensions' => $dimensions,
+            'type' => $type,
+            'dimensionFilterGroups' => $dimensionFilterGroups,
+            'aggregationType' => $aggregationType,
+        ];
+
+        do {
+            $response = $this->getSearchQueryResults(...$params);
+            if (!empty($response['rows']) && $callback) {
+                $callback($response['rows']);
+            }
+            $params['startRow'] += $params['rowLimit'];
+        } while (!empty($response['rows']) && count($response['rows']) == $params['rowLimit']);
+    }
+
+    /**
      * @throws GuzzleException
      * @link https://developers.google.com/search-console/api/reference/rest/v1/sites/list
      */
